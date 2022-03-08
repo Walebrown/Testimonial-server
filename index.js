dbe547d121d7c845e8e0ccb3994136e35761ff13
DevDewale <adewaleakangbe14@gmail.com>
2022-03-08 22:26:58 +0100

    made some adjustment in the env file

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import  express  from "express";
 import mongoose  from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from 'dotenv'
 
+dotenv.config({ path: './.env' });
 
 import postRoutes from "./routes/posts.js";
 
@@ -17,7 +19,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const MONGO_URL = "mongodb+srv://Adewale:Adewale@cluster0.1va7l.mongodb.net/testimonials?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
  const PORT = process.env.PORT || 5000;
 
  mongoose.connect(MONGO_URL, {useNewUrlParser: true, useUnifiedTopology:true})
